76ccb58915c1da5f7b5a49af3953c3624e68363c
YSLCoat <torstein.forseth@outlook.com>
2025-08-27 21:21:52 +0200

    Started work on trainer class. removed some comments

--- train.py
+++ train.py
@@ -72,14 +72,9 @@ def main(args):
         ngpus_per_node = 1
 
     if args.multiprocessing_distributed:
-        # Since we have ngpus_per_node processes per node, the total world_size
-        # needs to be adjusted accordingly
         args.world_size = ngpus_per_node * args.world_size
-        # Use torch.multiprocessing.spawn to launch distributed processes: the
-        # main_worker process function
         mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
     else:
-        # Simply call main_worker function
         main_worker(args.gpu, ngpus_per_node, args)
 
 
@@ -100,8 +95,6 @@ def main_worker(gpu, ngpus_per_node, args):
         if args.dist_url == "env://" and args.rank == -1:
             args.rank = int(os.environ["RANK"])
         if args.multiprocessing_distributed:
-            # For multiprocessing distributed training, rank needs to be the
-            # global rank among all the processes
             args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                 world_size=args.world_size, rank=args.rank)
@@ -112,26 +105,17 @@ def main_worker(gpu, ngpus_per_node, args):
     if not use_accel:
         logging.info('using CPU, this will be slow')
     elif args.distributed:
-        # For multiprocessing distributed, DistributedDataParallel constructor
-        # should always set the single device scope, otherwise,
-        # DistributedDataParallel will use all available devices.
         if device.type == 'cuda':
             if args.gpu is not None:
                 torch.cuda.set_device(args.gpu)
                 model.cuda(device)
-                # When using a single GPU per process and per
-                # DistributedDataParallel, we need to divide the batch size
-                # ourselves based on the total number of GPUs of the current node.
                 args.batch_size = int(args.batch_size / ngpus_per_node)
                 args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
                 model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
             else:
                 model.cuda()
-                # DistributedDataParallel will divide and allocate batch_size to all
-                # available GPUs if device_ids are not set
                 model = torch.nn.parallel.DistributedDataParallel(model)
     elif device.type == 'cuda':
-        # DataParallel will divide and allocate batch_size to all available GPUs
         if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
             model.features = torch.nn.DataParallel(model.features)
             model.cuda()
@@ -197,13 +181,10 @@ def main_worker(gpu, ngpus_per_node, args):
         if args.distributed:
             train_sampler.set_epoch(epoch)
 
-        # train for one epoch
         train(train_loader, model, criterion, optimizer, scheduler, epoch, device, args)
 
-        # evaluate on validation set
         acc1 = validate(val_loader, model, criterion, args)
         
-        # remember best acc@1 and save checkpoint
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
 
@@ -233,19 +214,15 @@ def train(train_loader, model, criterion, optimizer, scheduler, epoch, device, a
         [batch_time, data_time, losses, top1, top5],
         prefix="Epoch: [{}]".format(epoch))
 
-    # switch to train mode
     model.train()
 
     end = time.time()
     for i, (images, target) in enumerate(train_loader):
-        # measure data loading time
         data_time.update(time.time() - end)
 
-        # move data to the same device as model
         images = images.to(device, non_blocking=True)
         target = target.to(device, non_blocking=True)
 
-        # compute output
         output = model(images)
         loss = criterion(output, target)
 
@@ -253,20 +230,17 @@ def train(train_loader, model, criterion, optimizer, scheduler, epoch, device, a
         if args.mixup:
             target = target.argmax(dim=1)
 
-        # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
         losses.update(loss.item(), images.size(0))
         top1.update(acc1[0], images.size(0))
         top5.update(acc5[0], images.size(0))
 
-        # compute gradient and do optimizer step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         scheduler.step()
 
-        # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
 
@@ -298,17 +272,14 @@ def validate(val_loader, model, criterion, args):
                         images = images.to(device)
                         target = target.to(device)
 
-                # compute output
                 output = model(images)
                 loss = criterion(output, target)
 
-                # measure accuracy and record loss
                 acc1, acc5 = accuracy(output, target, topk=(1, 5))
                 losses.update(loss.item(), images.size(0))
                 top1.update(acc1[0], images.size(0))
                 top5.update(acc5[0], images.size(0))
 
-                # measure elapsed time
                 batch_time.update(time.time() - end)
                 end = time.time()
 
@@ -324,7 +295,6 @@ def validate(val_loader, model, criterion, args):
         [batch_time, losses, top1, top5],
         prefix='Test: ')
 
-    # switch to evaluate mode
     model.eval()
 
     run_validate(val_loader)
---
+++ trainer.py
@@ -0,0 +1,157 @@
+import os
+import random
+import time
+import warnings
+import logging
+import argparse
+
+from metrics import accuracy
+
+import torch
+from torch.utils.data import Subset
+from utils.training_utils import (
+    save_checkpoint,
+    AverageMeter,
+    ProgressMeter,
+    Summary
+)
+
+class Trainer():
+    def __init__(self):
+        pass
+
+
+    def train_epoch(self, train_loader: torch.utils.data.DataLoader, model: torch.nn.Module, criterion: torch.nn.Module, optimizer: torch.optim.Optimizer, scheduler: torch.optim.lr_scheduler, epoch: int, device, args: argparse.ArgumentParser):
+        use_accel = not args.no_accel and torch.accelerator.is_available()
+
+        batch_time = AverageMeter('Time', use_accel, ':6.3f', Summary.NONE)
+        data_time = AverageMeter('Data', use_accel, ':6.3f', Summary.NONE)
+        losses = AverageMeter('Loss', use_accel, ':.4e', Summary.NONE)
+        top1 = AverageMeter('Acc@1', use_accel, ':6.2f', Summary.NONE)
+        top5 = AverageMeter('Acc@5', use_accel, ':6.2f', Summary.NONE)
+        progress = ProgressMeter(
+            len(train_loader),
+            [batch_time, data_time, losses, top1, top5],
+            prefix="Epoch: [{}]".format(epoch))
+
+        model.train()
+
+        end = time.time()
+        for i, (images, target) in enumerate(train_loader):
+            data_time.update(time.time() - end)
+
+            images = images.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
+
+            output = model(images)
+            loss = criterion(output, target)
+
+            # select dominant label for accuracy calculation when using mixup
+            if args.mixup:
+                target = target.argmax(dim=1)
+
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            scheduler.step()
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i + 1)
+
+
+    def validate(self, val_loader: torch.utils.data.DataLoader, model: torch.nn.Module, criterion: torch.nn.Module, args: argparse.ArgumentParser):
+        use_accel = not args.no_accel and torch.accelerator.is_available()
+
+        def run_validate(loader, base_progress=0):
+
+            if use_accel:
+                device = torch.accelerator.current_accelerator()
+            else:
+                device = torch.device("cpu")
+
+            with torch.no_grad():
+                end = time.time()
+                for i, (images, target) in enumerate(loader):
+                    i = base_progress + i
+                    if use_accel:
+                        if args.gpu is not None and device.type=='cuda':
+                            torch.accelerator.set_device_index(args.gpu)
+                            images = images.cuda(args.gpu, non_blocking=True)
+                            target = target.cuda(args.gpu, non_blocking=True)
+                        else:
+                            images = images.to(device)
+                            target = target.to(device)
+
+                    output = model(images)
+                    loss = criterion(output, target)
+
+                    acc1, acc5 = accuracy(output, target, topk=(1, 5))
+                    losses.update(loss.item(), images.size(0))
+                    top1.update(acc1[0], images.size(0))
+                    top5.update(acc5[0], images.size(0))
+
+                    batch_time.update(time.time() - end)
+                    end = time.time()
+
+                    if i % args.print_freq == 0:
+                        progress.display(i + 1)
+
+        batch_time = AverageMeter('Time', use_accel, ':6.3f', Summary.NONE)
+        losses = AverageMeter('Loss', use_accel, ':.4e', Summary.NONE)
+        top1 = AverageMeter('Acc@1', use_accel, ':6.2f', Summary.AVERAGE)
+        top5 = AverageMeter('Acc@5', use_accel, ':6.2f', Summary.AVERAGE)
+        progress = ProgressMeter(
+            len(val_loader) + (args.distributed and (len(val_loader.sampler) * args.world_size < len(val_loader.dataset))),
+            [batch_time, losses, top1, top5],
+            prefix='Test: ')
+
+        model.eval()
+
+        run_validate(val_loader)
+        if args.distributed:
+            top1.all_reduce()
+            top5.all_reduce()
+
+        if args.distributed and (len(val_loader.sampler) * args.world_size < len(val_loader.dataset)):
+            aux_val_dataset = Subset(val_loader.dataset,
+                                    range(len(val_loader.sampler) * args.world_size, len(val_loader.dataset)))
+            aux_val_loader = torch.utils.data.DataLoader(
+                aux_val_dataset, batch_size=args.batch_size, shuffle=False,
+                num_workers=args.workers, pin_memory=True)
+            run_validate(aux_val_loader, len(val_loader))
+
+        progress.display_summary()
+
+        return top1.avg
+    
+    def train(self):
+        for epoch in range(self.args.start_epoch, self.args.epochs):
+            if self.args.distributed:
+                self.train_sampler.set_epoch(epoch)
+
+            self.train_epoch(self.train_loader, self.model, self.criterion, self.optimizer, self.scheduler, self.epoch, self.device, self.args)
+
+            acc1 = self.validate(self.val_loader, self.model, self.criterion, self.args)
+            
+            is_best = acc1 > best_acc1
+            best_acc1 = max(acc1, best_acc1)
+
+            if not self.args.multiprocessing_distributed or (self.args.multiprocessing_distributed
+                    and self.args.rank % self.ngpus_per_node == 0):
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'arch': self.args.arch,
+                    'state_dict': self.model.state_dict(),
+                    'best_acc1': best_acc1,
+                    'optimizer' : self.optimizer.state_dict(),
+                    'scheduler' : self.scheduler.state_dict()
+                }, is_best)
